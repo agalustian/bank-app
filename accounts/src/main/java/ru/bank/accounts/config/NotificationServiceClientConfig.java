df8eaf936c0af282af12f4915413c096b988c7f6
agalustian <agalustian@yahoo.com>
2026-01-14 01:03:03 +0300

    feat (sprint-9): add notification client generation

---
+++ accounts/src/main/java/ru/bank/accounts/config/NotificationServiceClientConfig.java
@@ -0,0 +1,26 @@
+package ru.bank.accounts.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+import ru.bank.accounts.notifications.client.ApiClient;
+import ru.bank.accounts.notifications.client.api.NotificationsControllerApi;
+
+@Configuration
+public class NotificationServiceClientConfig {
+
+  @Bean
+  ApiClient apiClient(RestTemplate restTemplate) {
+    var client = new ApiClient(restTemplate);
+    // TODO use from env
+    client.setBasePath("http://localhost:8082");
+
+    return client;
+  }
+
+  @Bean
+  NotificationsControllerApi notificationsControllerApi(ApiClient apiClient) {
+    return new NotificationsControllerApi();
+  }
+
+}
---
+++ accounts/src/main/java/ru/bank/accounts/services/NotificationsService.java
@@ -0,0 +1,24 @@
+package ru.bank.accounts.services;
+
+import org.springframework.stereotype.Service;
+import ru.bank.accounts.notifications.client.api.NotificationsControllerApi;
+import ru.bank.accounts.notifications.domain.NotificationDTO;
+
+@Service
+public class NotificationsService {
+
+  private final NotificationsControllerApi notificationsControllerApi;
+
+  public NotificationsService(NotificationsControllerApi notificationsControllerApi) {
+    this.notificationsControllerApi = notificationsControllerApi;
+  }
+
+  void notify(String username, String text) {
+    var notification = new NotificationDTO();
+    notification.setUsername(username);
+    notification.setText(text);
+
+    notificationsControllerApi.sendNotification(notification);
+  }
+
+}
