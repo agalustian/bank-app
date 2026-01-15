c2cab41f867bc7f035de91be04efb108ce281fa3
agalustian <agalustian@yahoo.com>
2026-01-15 17:00:03 +0300

    test (sprint-9): add accounts security tests

--- accounts/src/test/java/ru/bank/accounts/integration/controllers/AccountsControllerTests.java
+++ accounts/src/test/java/ru/bank/accounts/integration/controllers/AccountsControllerTests.java
@@ -2,6 +2,7 @@ package ru.bank.accounts.integration.controllers;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
@@ -11,6 +12,7 @@ import java.util.List;
 import java.util.UUID;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MvcResult;
 import ru.bank.accounts.controllers.AccountsController;
@@ -52,20 +54,21 @@ class AccountsControllerTests {
   }
 
   @Test
+  @WithMockUser(roles = "ACCOUNTS", username = "test")
   void shouldGetAccount() throws Exception {
-    var accountDTO = new AccountDTO(UUID.randomUUID(),  "test", "fullname", "1988-10-23", 10);
+    var accountDTO = new AccountDTO(UUID.randomUUID(), "test", "fullname", "1988-10-23", 10);
 
-    when(accountsService.getAccountByLogin("stub")).thenReturn(accountDTO);
+    when(accountsService.getAccountByLogin("test")).thenReturn(accountDTO);
 
     MvcResult response = mockMvc.perform(get("/v1/accounts"))
         .andExpect(status().isOk())
-        .andExpect(content().contentType(MediaType.APPLICATION_JSON))
         .andReturn();
 
     assertEquals(accountToJSON(accountDTO), response.getResponse().getContentAsString());
   }
 
   @Test
+  @WithMockUser(roles = "ACCOUNTS")
   void shouldGetAccountsShortInfo() throws Exception {
     var accountShortInfoDTOList = List.of(new AccountShortInfoDTO("test", "fullname"));
 
@@ -80,12 +83,14 @@ class AccountsControllerTests {
   }
 
   @Test
+  @WithMockUser(roles = "ACCOUNTS")
   void shouldUpdateAccount() throws Exception {
-    var accountDTO = new AccountDTO(UUID.randomUUID(), "test",  "fullname", "1988-10-23", 10);
+    var accountDTO = new AccountDTO(UUID.randomUUID(), "test", "fullname", "1988-10-23", 10);
 
-    when(accountsService.updateAccountByLogin("stub", accountDTO)).thenReturn(accountDTO);
+    when(accountsService.updateAccountByLogin("test", accountDTO)).thenReturn(accountDTO);
 
-    mockMvc.perform(put("/v1/accounts").contentType(MediaType.APPLICATION_JSON).content(accountToJSON(accountDTO)))
+    mockMvc.perform(
+            put("/v1/accounts/test").with(csrf()).contentType(MediaType.APPLICATION_JSON).content(accountToJSON(accountDTO)))
         .andExpect(status().isOk());
   }
 
