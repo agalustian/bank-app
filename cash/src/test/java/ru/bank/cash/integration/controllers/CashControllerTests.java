980fa6c2198763d35ac86756b6d908a124c087c0
agalustian <agalustian@yahoo.com>
2026-01-15 16:55:50 +0300

    test (sprint-9): add security tests for cash service

--- cash/src/test/java/ru/bank/cash/integration/controllers/CashControllerTests.java
+++ cash/src/test/java/ru/bank/cash/integration/controllers/CashControllerTests.java
@@ -1,5 +1,6 @@
 package ru.bank.cash.integration.controllers;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -9,6 +10,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 import ru.bank.cash.controllers.CashController;
@@ -36,19 +38,37 @@ class CashControllerTests {
   }
 
   @Test
+  @WithMockUser(roles = "CASH")
   void shouldDepositMoney() throws Exception {
     var editMoneyDTO = new EditMoneyDTO(100);
 
-    mockMvc.perform(put("/v1/cash/deposit").content(toJSON(editMoneyDTO)).contentType(MediaType.APPLICATION_JSON))
+    mockMvc.perform(put("/v1/cash/deposit").with(csrf()).content(toJSON(editMoneyDTO)).contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNoContent());
   }
 
   @Test
+  void shouldThrowAnErrorOnDeposit() throws Exception {
+    var editMoneyDTO = new EditMoneyDTO(100);
+
+    mockMvc.perform(put("/v1/cash/deposit").with(csrf()).content(toJSON(editMoneyDTO)).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().is3xxRedirection());
+  }
+
+  @Test
+  @WithMockUser(roles = "CASH")
   void shouldWithdrawalMoney() throws Exception {
     var editMoneyDTO = new EditMoneyDTO(100);
 
-    mockMvc.perform(put("/v1/cash/withdrawal").content(toJSON(editMoneyDTO)).contentType(MediaType.APPLICATION_JSON))
+    mockMvc.perform(put("/v1/cash/withdrawal").with(csrf()).content(toJSON(editMoneyDTO)).contentType(MediaType.APPLICATION_JSON))
         .andExpect(status().isNoContent());
+  }
+
+  @Test
+  void shouldThrowAnErrorOnWithdrawal() throws Exception {
+    var editMoneyDTO = new EditMoneyDTO(100);
+
+    mockMvc.perform(put("/v1/cash/withdrawal").with(csrf()).content(toJSON(editMoneyDTO)).contentType(MediaType.APPLICATION_JSON))
+        .andExpect(status().is3xxRedirection());
   }
 
 }
