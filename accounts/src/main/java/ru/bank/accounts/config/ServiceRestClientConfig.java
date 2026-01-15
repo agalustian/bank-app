67b000cf631118ef07e100ece10f802ec05eb908
agalustian <agalustian@yahoo.com>
2026-01-16 00:23:49 +0300

    fix (spring-9): repair service discovery and sending notifications from accounts to notifications service

---
+++ accounts/src/main/java/ru/bank/accounts/config/ServiceRestClientConfig.java
@@ -0,0 +1,54 @@
+package ru.bank.accounts.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.client.AuthorizedClientServiceOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProvider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.client.OAuth2ClientHttpRequestInterceptor;
+import org.springframework.web.client.RestClient;
+
+@Configuration
+public class ServiceRestClientConfig {
+  @LoadBalanced
+  @Bean
+  RestClient.Builder restClientBuilder() {
+    return RestClient.builder();
+  }
+
+  @Bean
+  @LoadBalanced
+  public RestClient serviceRestClient(RestClient.Builder builder, OAuth2AuthorizedClientManager authorizedClientManager) {
+    OAuth2ClientHttpRequestInterceptor requestInterceptor =
+        new OAuth2ClientHttpRequestInterceptor(authorizedClientManager);
+
+    requestInterceptor.setClientRegistrationIdResolver((request) -> "accounts-service");
+
+    return builder.requestInterceptor(requestInterceptor).build();
+  }
+
+  @Bean
+  public OAuth2AuthorizedClientManager authorizedClientManager(
+      ClientRegistrationRepository clientRegistrationRepository,
+      OAuth2AuthorizedClientService authorizedClientService
+  ) {
+    OAuth2AuthorizedClientProvider authorizedClientProvider =
+        OAuth2AuthorizedClientProviderBuilder.builder()
+            .clientCredentials()
+            .build();
+
+    AuthorizedClientServiceOAuth2AuthorizedClientManager manager =
+        new AuthorizedClientServiceOAuth2AuthorizedClientManager(
+            clientRegistrationRepository,
+            authorizedClientService
+        );
+
+    manager.setAuthorizedClientProvider(authorizedClientProvider);
+    return manager;
+  }
+
+}
