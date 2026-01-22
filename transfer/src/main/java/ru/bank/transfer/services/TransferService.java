f131678232ecdce77a7d7aa7bb06590ba6eea44e
agalustian <agalustian@yahoo.com>
2026-01-23 00:17:20 +0300

    feat (sprint-12): add transfer error metric

--- transfer/src/main/java/ru/bank/transfer/services/TransferService.java
+++ transfer/src/main/java/ru/bank/transfer/services/TransferService.java
@@ -1,5 +1,6 @@
 package ru.bank.transfer.services;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -21,26 +22,36 @@ public class TransferService {
 
   private final DepositOutboxJpaRepository depositOutboxJpaRepository;
 
+  private final MeterRegistry meterRegistry;
+
   public TransferService(AccountsService accountsService,
                          NotificationsOutboxJpaRepository notificationsOutboxJpaRepository,
-                         DepositOutboxJpaRepository depositOutboxJpaRepository
+                         DepositOutboxJpaRepository depositOutboxJpaRepository,
+                         MeterRegistry meterRegistry
   ) {
     this.accountsService = accountsService;
     this.notificationsOutboxJpaRepository = notificationsOutboxJpaRepository;
     this.depositOutboxJpaRepository = depositOutboxJpaRepository;
+    this.meterRegistry = meterRegistry;
   }
 
   @Transactional
   public void transfer(String login, TransferDTO transferDTO) {
-    var fromAccount = accountsService.getAccount();
+    try {
+      var fromAccount = accountsService.getAccount();
 
-    accountsService.withdrawal(fromAccount, transferDTO.amount());
-    notificationsOutboxJpaRepository.save(
-        new NotificationOutbox("Withdrawal money success", fromAccount.getFullname()));
+      accountsService.withdrawal(fromAccount, transferDTO.amount());
+      notificationsOutboxJpaRepository.save(
+          new NotificationOutbox("Withdrawal money success", fromAccount.getFullname()));
 
-    depositOutboxJpaRepository.save(new DepositOutbox(login, transferDTO.to(), transferDTO.amount()));
+      depositOutboxJpaRepository.save(new DepositOutbox(login, transferDTO.to(), transferDTO.amount()));
 
-    logger.info("Transfer successful for: {}", login);
+      logger.info("Transfer successful for: {}", login);
+    } catch (RuntimeException e) {
+      meterRegistry.counter("transfer_error", "from", login, "to", transferDTO.to()).increment();
+
+      throw e;
+    }
   }
 
 }
