a11a303b549d4b156d8e1f167e4b245fed6abe9e
agalustian <agalustian@yahoo.com>
2026-01-22 03:11:35 +0300

    feat (sprint-12): upd chassis version in all modules

--- notifications/src/main/java/ru/bank/notifications/services/NotificationsService.java
+++ notifications/src/main/java/ru/bank/notifications/services/NotificationsService.java
@@ -1,5 +1,6 @@
 package ru.bank.notifications.services;
 
+import io.micrometer.core.instrument.MeterRegistry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
@@ -10,10 +11,19 @@ public class NotificationsService {
 
   private static final Logger logger = LoggerFactory.getLogger(NotificationsService.class);
 
+  private final MeterRegistry meterRegistry;
+
+  public NotificationsService(MeterRegistry meterRegistry) {
+    this.meterRegistry = meterRegistry;
+  }
+
   public void sendNotification(NotificationDTO notification) {
     if (notification == null) {
+      meterRegistry.counter("send_notification_error", "type", "logger").increment();
       return;
     }
+
+    meterRegistry.counter("send_notification_total", "type", "logger").increment();
 
     logger.info("Notification for '{}' with text '{}'", notification.username(), notification.text());
   }
